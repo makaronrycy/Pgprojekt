ca61c3b408974a2f9e32947ce5b10990997622a3
Maurycy Szmuc <mauryc_s@wp.pl>
2023-02-27 20:39:22 +0100

    baza

--- Pgprojekt/Pgprojekt.cpp
+++ Pgprojekt/Pgprojekt.cpp
@@ -2,9 +2,23 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/*Segmenty ID:
+    A = 0
+    B = 1 
+    C = 2 
+    D = 3
+    E = 4
+    F = 5
+*/
+
+const int widthOfMap = 8;
+const int heightOfMap = 4;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 

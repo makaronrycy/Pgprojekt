f650444c0c63da51c17dbd2844f1509b1ac8aef2
Maurycy Szmuc <mauryc_s@wp.pl>
2023-02-27 17:32:09 +0100

    Dodaj pliki projektów.

---
+++ Pgprojekt/Pgprojekt.cpp
@@ -0,0 +1,10 @@
+﻿// Pgprojekt.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+

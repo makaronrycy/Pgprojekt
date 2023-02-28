ca18c83059ecf35ce9be281dbd8988c2df2b70c0
Maurycy Szmuc <mauryc_s@wp.pl>
2023-02-28 20:41:49 +0100

    Podstawowe rysowanie planszy, utworzenie segmentu A

--- Pgprojekt/Pgprojekt.cpp
+++ Pgprojekt/Pgprojekt.cpp
@@ -3,8 +3,20 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 using namespace std;
 
+
+
+const char blackSquare = char(219);
+const char whiteSquare = ' ';
+const int A[5][5] = {
+    {1,1,0,1,1},
+    {1,0,0,1,1},
+    {0,0,1,0,0},
+    {1,0,0,0,1},
+    {1,1,0,1,1}
+};
 /*Segmenty ID:
     A = 0
     B = 1 
@@ -14,11 +26,31 @@ using namespace std;
     F = 5
 */
 
-const int widthOfMap = 8;
-const int heightOfMap = 4;
+const int widthOfMap = 1;
+const int heightOfMap = 1;
+void drawBoard(int map[][5]) {
+    cout << setw(0);
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            switch (A[i][j])
+            {
+            case 1:
+                cout << blackSquare;
+                break;
+            case 0:
+                cout << whiteSquare;
+                break;
+            }
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    cout << "Hello World!\n";
+    int map[5][5];
+    drawBoard(map);
 }
 
